fd9c4ea67b279f1a5063497d7e9e956458903531
Alper <asunar.git@gmail.com>
2017-03-31 06:32:47 -0500

    Apply LeNet network

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import numpy as np
 
 lines = []
 
-with open ('data/data/driving_log.csv') as csvfile:
+with open ('data/driving_log.csv') as csvfile:
     next(csvfile)
     reader = csv.reader(csvfile)
     for line in reader:
@@ -24,5 +24,29 @@ for line in lines:
 X_train = np.array(images)
 y_train = np.array(measurements)
 
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D
 
-print(y_train[100])
+model = Sequential()
+
+# Normalizing lambda layer
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+
+# First convolutional layer
+model.add(Convolution2D(6,5,5,activation='relu'))
+model.add(MaxPooling2D())
+
+# Second convolutional layer
+model.add(Convolution2D(6,5,5,activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+
+# Fully-connected layers
+model.add(Dense(120))
+model.add(Dense(84))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle= True, nb_epoch=6)
+
+model.save('model.h5')

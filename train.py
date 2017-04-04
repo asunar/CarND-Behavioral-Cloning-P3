8987b395159964749e4729534da32d8dd2b42d71
Alper <asunar.git@gmail.com>
2017-04-04 06:53:48 -0500

    Add nvidia model

--- train.py
+++ train.py
@@ -134,24 +134,19 @@ def get_training_data(lines):
 def build_model():
     model = Sequential()
 
-    # Normalizing lambda layer
+    #Nvidia model
+    model = Sequential()
     model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-
-    #crop
-    model.add(Cropping2D(cropping=((70, 25), (1, 1))))
-
-    # First convolutional layer
-    model.add(Convolution2D(6,5,5,activation='relu'))
-    model.add(MaxPooling2D())
-
-    # Second convolutional layer
-    model.add(Convolution2D(6,5,5,activation='relu'))
-    model.add(MaxPooling2D())
+    model.add(Cropping2D(cropping=((70,25),(1,1))))
+    model.add(Convolution2D(24,5,5, subsample=(2,2),activation='relu'))
+    model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
+    model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
+    model.add(Convolution2D(64,3,3,activation='relu'))
+    model.add(Convolution2D(64,3,3,activation='relu'))
     model.add(Flatten())
-
-    # Fully-connected layers
-    model.add(Dense(120))
-    model.add(Dense(84))
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
     model.add(Dense(1))
 
     model.compile(loss='mse', optimizer='adam')

f29c033afa27b919ded388b06ae8c06e14776163
Alper <asunar.git@gmail.com>
2017-04-04 06:32:03 -0500

    Train with augmented images, crop

--- train.py
+++ train.py
@@ -1,38 +1,40 @@
-import csv
+
 import cv2
+
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D,Cropping2D
 
 
 def get_logs(path):
+    import csv
     lines = []
     with open (path) as csvfile:
         next(csvfile)
         reader = csv.reader(csvfile)
         return [line for line in reader]
 
-def get_images_measurements(lines):
-    images = []
-    measurements = []
+# def get_images_measurements(lines):
+#     images = []
+#     measurements = []
 
-    for line in lines:
-        source_path = line[0]
-        tokens = source_path.split('/')
-        filename = tokens[-1]
-        local_path = "./data/IMG/" + filename
-        image = cv2.imread(local_path)
-        images.append(image)
-        measurement = line[3]
-        measurements.append(measurement)
+#     for line in lines:
+#         source_path = line[0]
+#         tokens = source_path.split('/')
+#         filename = tokens[-1]
+#         local_path = "./data/IMG/" + filename
+#         image = cv2.imread(local_path)
+#         images.append(image)
+#         measurement = line[3]
+#         measurements.append(measurement)
     
-    print("Images count:" + str(len(images)))
+#     print("Images count:" + str(len(images)))
 
-    images_array = np.array(images[1:])
-    measurements_array = np.array(measurements[1:])    
-    images_measurements = (images_array, measurements_array)   
+    # images_array = np.array(images[1:])
+    # measurements_array = np.array(measurements[1:])    
+    # images_measurements = (images_array, measurements_array)   
 
-    return images_measurements 
+    # return images_measurements 
 
 
 
@@ -76,21 +78,67 @@ def get_left_right_camera_images_measurements_with_correction(lines):
         # Minus correction for steering for right images
         measurements.append(measurement-correction)
 
+    print("Lines count:" + str(len(lines)))
+    print("Measurements count:" + str(len(measurements)))
+
+    assert len(lines*3) == len(measurements), 'Actual number of *jpg images does not match with the csv log file'
     return (images, measurements)
 
 def get_training_data(lines):
-    images_measurements = get_images_measurements(lines)
-    augmented_images_measurements = augment_images(images_measurements[0], images_measurements[1])
-    left_right_camera_images_measurements_with_correction = get_left_right_camera_images_measurements_with_correction(lines)
+    images = []
+    measurements = []
+    for line in lines:
+        for i in range(3):
+            # Load images from center, left and right cameras
+            source_path = line[i]
+            tokens = source_path.split('/')
+            filename = tokens[-1]
+            local_path = "./data/IMG/" + filename
+            image = cv2.imread(local_path)
+            images.append(image)
+        correction = 0.2
+        measurement = float(line[3])
+        
+        # Steering adjustment for center images
+        measurements.append(measurement)
+        
+        # Add correction for steering for left images
+        measurements.append(measurement+correction)
+        
+        # Minus correction for steering for right images
+        measurements.append(measurement-correction)
 
+    augmented_images = []
+    augmented_measurements = []
+    for image, measurement in zip(images, measurements):
+        augmented_images.append(image)
+        augmented_measurements.append(measurement)
+        flipped_image = cv2.flip(image, 1)
+        flipped_measurement = float(measurement) * -1.0
+        augmented_images.append(flipped_image)
+        augmented_measurements.append(flipped_measurement)
 
-    return augmented_images_measurements
+    X_train = np.array(augmented_images)
+    y_train = np.array(augmented_measurements)    
+
+    # corrected_side_images = get_left_right_camera_images_measurements_with_correction(lines)
+    # augmented_images_measurements = augment_images(corrected_side_images[0], corrected_side_images[1])
+
+    # X_train = np.concatenate(augmented_images_measurements[0],
+    #                 corrected_side_images[0])
+    # y_train = np.concatenate(augmented_images_measurements[1],
+    #                 corrected_side_images[1])
+
+    return (X_train, y_train)
 
 def build_model():
     model = Sequential()
 
     # Normalizing lambda layer
     model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+
+    #crop
+    model.add(Cropping2D(cropping=((70, 25), (1, 1))))
 
     # First convolutional layer
     model.add(Convolution2D(6,5,5,activation='relu'))
@@ -116,9 +164,12 @@ def run_model(model, X_train,y_train):
     print("Model fit complete, saving model")
     model.save('model.h5')
 
-logs = get_logs('data/driving_log.csv')
-training_data = get_training_data(logs)
-X_train, y_train = training_data[0], training_data[1]
-model = build_model()
+def train():
+    logs = get_logs('data/driving_log.csv')
+    training_data = get_training_data(logs)
+    X_train, y_train = training_data[0], training_data[1]
+    model = build_model()
 
-run_model(model, X_train, y_train)
+    run_model(model, X_train, y_train)
+
+train()

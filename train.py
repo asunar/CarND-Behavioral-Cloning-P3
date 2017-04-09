19e2065133b35404bb851e0d161c9f44145506ca
Alper <asunar.git@gmail.com>
2017-04-09 06:59:11 -0500

    Add my training data

--- train.py
+++ train.py
@@ -14,47 +14,7 @@ def get_logs(path):
         reader = csv.reader(csvfile)
         return [line for line in reader]
 
-# def get_images_measurements(lines):
-#     images = []
-#     measurements = []
-
-#     for line in lines:
-#         source_path = line[0]
-#         tokens = source_path.split('/')
-#         filename = tokens[-1]
-#         local_path = "./data/IMG/" + filename
-#         image = cv2.imread(local_path)
-#         images.append(image)
-#         measurement = line[3]
-#         measurements.append(measurement)
-    
-#     print("Images count:" + str(len(images)))
-
-    # images_array = np.array(images[1:])
-    # measurements_array = np.array(measurements[1:])    
-    # images_measurements = (images_array, measurements_array)   
-
-    # return images_measurements 
-
-
-
-def augment_images(images, measurements):
-    augmented_images = []
-    augmented_measurements = []
-    for image, measurement in zip(images, measurements):
-        augmented_images.append(image)
-        augmented_measurements.append(measurement)
-        flipped_image = cv2.flip(image, 1)
-        flipped_measurement = float(measurement) * -1.0
-        augmented_images.append(flipped_image)
-        augmented_measurements.append(flipped_measurement)
-
-    augmented_images_array = np.array(augmented_images)
-    augmented_measurements_array = np.array(augmented_measurements)
-
-    return (augmented_images_array, augmented_measurements_array) 
-
-def get_left_right_camera_images_measurements_with_correction(lines):
+def get_training_data(lines, local_image_path):
     images = []
     measurements = []
     for line in lines:
@@ -63,37 +23,7 @@ def get_left_right_camera_images_measurements_with_correction(lines):
             source_path = line[i]
             tokens = source_path.split('/')
             filename = tokens[-1]
-            local_path = "./data/IMG/" + filename
-            image = cv2.imread(local_path)
-            images.append(image)
-        correction = 0.2
-        measurement = float(line[3])
-        
-        # Steering adjustment for center images
-        measurements.append(measurement)
-        
-        # Add correction for steering for left images
-        measurements.append(measurement+correction)
-        
-        # Minus correction for steering for right images
-        measurements.append(measurement-correction)
-
-    print("Lines count:" + str(len(lines)))
-    print("Measurements count:" + str(len(measurements)))
-
-    assert len(lines*3) == len(measurements), 'Actual number of *jpg images does not match with the csv log file'
-    return (images, measurements)
-
-def get_training_data(lines):
-    images = []
-    measurements = []
-    for line in lines:
-        for i in range(3):
-            # Load images from center, left and right cameras
-            source_path = line[i]
-            tokens = source_path.split('/')
-            filename = tokens[-1]
-            local_path = "./data/IMG/" + filename
+            local_path = local_image_path + filename
             image = cv2.imread(local_path)
             images.append(image)
         correction = 0.2
@@ -120,14 +50,6 @@ def get_training_data(lines):
 
     X_train = np.array(augmented_images)
     y_train = np.array(augmented_measurements)    
-
-    # corrected_side_images = get_left_right_camera_images_measurements_with_correction(lines)
-    # augmented_images_measurements = augment_images(corrected_side_images[0], corrected_side_images[1])
-
-    # X_train = np.concatenate(augmented_images_measurements[0],
-    #                 corrected_side_images[0])
-    # y_train = np.concatenate(augmented_images_measurements[1],
-    #                 corrected_side_images[1])
 
     return (X_train, y_train)
 
@@ -160,9 +82,13 @@ def run_model(model, X_train,y_train):
     model.save('model.h5')
 
 def train():
-    logs = get_logs('data/driving_log.csv')
-    training_data = get_training_data(logs)
-    X_train, y_train = training_data[0], training_data[1]
+    sample_log = get_logs('data/driving_log.csv')
+    my_log = get_logs('my_training_data/driving_log.csv')
+    sample_training_data = get_training_data(sample_log, "./data/IMG/")
+    my_training_data = get_training_data(my_log, "./my_training_data/IMG/")
+    X_train = np.concatenate([sample_training_data[0], my_training_data[0]]) 
+    y_train = np.concatenate([sample_training_data[1], my_training_data[1]]) 
+    
     model = build_model()
 
     run_model(model, X_train, y_train)

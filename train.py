9f4a22d9f5c44f4e97f06a8f27d21b7786e51f75
Alper <asunar.git@gmail.com>
2017-04-15 18:30:37 -0500

    Add generator

--- train.py
+++ train.py
@@ -1,22 +1,46 @@
 
 import cv2
-import cv2
 import sklearn
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D,Cropping2D
-
-
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D, Cropping2D
+import sklearn.utils
+from sklearn.model_selection import train_test_split
 
 def get_logs(path):
     import csv
-    import itertools
-    lines = []
-    with open (path) as csvfile:
+
+    samples = []
+    with open(path) as csvfile:
         next(csvfile)
         reader = csv.reader(csvfile)
-        # return [line for line in reader]
-        return itertools.islice([line for line in reader], 9000)
+        for line in reader:
+            samples.append(line)
+
+    train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+    return (train_samples, validation_samples)
+        
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        # shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = 'data/IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
 
 def get_training_data(lines, local_image_path):
     images = []
@@ -83,29 +107,27 @@ def build_model():
     print("Built the model")
     return model
 
-def run_model(model, X_train,y_train):
-    model.fit(X_train, y_train, validation_split=0.2, shuffle= True, nb_epoch=6)
-    print("Model fit complete, saving model")
-    model.save('model.h5')
+def run_model(model, train_samples, validation_samples):
+    # model.fit(X_train, y_train, validation_split=0.2, shuffle= True, nb_epoch=6)
+    # print("Model fit complete, saving model")
+    # model.save('model.h5')
+
+    train_generator = generator(train_samples, batch_size=32)
+    validation_generator = generator(validation_samples, batch_size=32)
+
+    model.fit_generator(train_generator, samples_per_epoch= 
+                len(train_samples), validation_data=validation_generator, 
+                nb_val_samples=len(validation_samples), nb_epoch=3)    
+
+def print_sample(list):
+    result = [x[0] + " - " + x[3] for x in list]
+    print(result) 
 
 def train():
-    sample_log = get_logs('data/driving_log.csv')
-    my_log = get_logs('my_training_data/driving_log.csv')
-
-    sample_training_data = get_training_data(sample_log, "./data/IMG/")
-    my_training_data = get_training_data(my_log, "./my_training_data/IMG/")
-
-    x = np.concatenate([sample_training_data[0], my_training_data[0]])
-    y = np.concatenate([sample_training_data[1], my_training_data[1]])
-    X_train, y_train = (x, y)
-    # X_train, y_train = (sample_training_data[0], sample_training_data[1])
-
-    print("X_train:" + str(len(X_train)))
-    print("y_train:" + str(len(y_train)))
+    train_samples, validation_samples = get_logs('data/driving_log.csv')
 
     model = build_model()
-
-    run_model(model, X_train, y_train)
-
+    run_model(model, train_samples, validation_samples)
 
 train()
+
